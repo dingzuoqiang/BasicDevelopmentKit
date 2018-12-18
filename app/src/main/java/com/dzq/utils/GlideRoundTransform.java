3bb58522ca2841e6986f0f3f99e76436673da25a
dingzuoqiang <530858106@qq.com>
2018-12-18 12:23:21 +0800

    圆角：GlideRoundTransform

---
+++ app/src/main/java/com/dzq/utils/GlideRoundTransform.java
@@ -0,0 +1,80 @@
+package com.dzq.utils;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils;
+
+import java.security.MessageDigest;
+
+/**
+ * Glide 圆角 Transform
+ * Created by dingzuoqiang on 2017/9/11.
+ * 530858106@qq.com
+ */
+
+public class GlideRoundTransform extends BitmapTransformation {
+
+    private static float radius = 0f;
+
+    /**
+     * 构造函数 默认圆角半径 4dp
+     *
+     * @param context Context
+     */
+    public GlideRoundTransform(Context context) {
+        this(context, 4);
+    }
+
+    /**
+     * 构造函数
+     *
+     * @param context Context
+     * @param dp      圆角半径
+     */
+    public GlideRoundTransform(Context context, int dp) {
+        super(context);
+        this.radius = Resources.getSystem().getDisplayMetrics().density * dp;
+    }
+
+    @Override
+    protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+        Bitmap bitmap = TransformationUtils.centerCrop(pool, toTransform, outWidth, outHeight);
+        return roundCrop(pool, bitmap);
+    }
+
+    private static Bitmap roundCrop(BitmapPool pool, Bitmap source) {
+        if (source == null) return null;
+
+        Bitmap result = pool.get(source.getWidth(), source.getHeight(), Bitmap.Config.ARGB_8888);
+        if (result == null) {
+            result = Bitmap.createBitmap(source.getWidth(), source.getHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(result);
+        Paint paint = new Paint();
+        paint.setShader(new BitmapShader(source, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP));
+        paint.setAntiAlias(true);
+        RectF rectF = new RectF(0f, 0f, source.getWidth(), source.getHeight());
+        canvas.drawRoundRect(rectF, radius, radius, paint);
+        return result;
+    }
+
+    public String getId() {
+        return getClass().getName() + Math.round(radius);
+    }
+
+    @Override
+    public void updateDiskCacheKey(MessageDigest messageDigest) {
+
+    }
+
+}
